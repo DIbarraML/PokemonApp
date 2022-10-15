3cacf1d9b2b7eb1a27adcc2dc3738edc0336a8f9
daibarra <daniel.ibarra@mercadolibre.com.co>
2022-10-15 03:28:18 -0500

    Se agrega el splashScreen

--- app/src/main/java/com/example/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_PokemonApp)
         setContentView(R.layout.activity_main)
     }
 }
